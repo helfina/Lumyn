7d7866073e61ff81e85f9723ef07bd212c205f12
helfina <gkerforne@gmail.com>
2026-07-21 18:13:24 +0200

    Fin du sprint 2 - Première application BeeWare

---
+++ prototype/lumyn/src/lumyn/app.py
@@ -0,0 +1,26 @@
+"""
+Assistant personnel modulaire pensé pour alléger la charge mentale.
+"""
+
+import toga
+from toga.style.pack import COLUMN, ROW
+
+
+class Lumyn(toga.App):
+    def startup(self):
+        """Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+        bonjour = toga.Label("Bonjour Gaëlle 👋")
+        main_box.add(bonjour)
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return Lumyn()
